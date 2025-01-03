77b2b0613a0079d3397a056e0a9e58a7f10b2fe0
David Desmarais-Michaud <desmarais.michaud.david@gmail.com>
2025-01-03 14:13:01 -0500

    updates

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,6 +21,11 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.telescope" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.lang.helm" },
+    { import = "lazyvim.plugins.extras.ui.treesitter-context" },
     -- import/override with your plugins
     { import = "plugins" },
   },

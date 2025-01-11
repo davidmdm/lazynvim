9dc41ecbdc3777dda6ebdde5b5c8c3ceb909e146
David Desmarais-Michaud <desmarais.michaud.david@gmail.com>
2025-01-11 10:31:13 -0500

    update deps

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,6 +23,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.zig" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.git" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.lang.helm" },

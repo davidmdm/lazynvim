b76e52799d54c2a23339dbbe047613ef2816be6f
David Desmarais-Michaud <desmarais.michaud.david@gmail.com>
2025-01-14 20:40:09 -0500

    remove telescope from extras since we use snack picker

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,7 +18,6 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    { import = "lazyvim.plugins.extras.editor.telescope" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.zig" },

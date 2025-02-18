c2e91bf2bc66aa4fb1aefa3132cd7f19c706891b
David Desmarais-Michaud <desmarais.michaud.david@gmail.com>
2025-02-17 22:03:38 -0500

    update deps

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -7,6 +7,7 @@ return {
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
       },
+      image = {},
       picker = {
         sources = {
           explorer = {

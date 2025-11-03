21da69e73c0306d4b4ed7481e2057cdf96652927
David Desmarais-Michaud <desmarais.michaud.david@gmail.com>
2025-11-02 20:38:04 -0500

    add git branches keymap gB

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,6 +8,7 @@ vim.keymap.set("n", "<leader>fw", "<cmd> Telescope live_grep <CR>", { desc = "li
 vim.keymap.set("n", "<leader>ud", "<cmd>lua Snacks.toggle.dim() <CR>", { desc = "toggle dim" })
 
 vim.keymap.set("n", "<leader>cw", "<cmd>lua Snacks.picker.diagnostics() <CR>", { desc = "workspace diagnostics" })
+vim.keymap.set("n", "<leader>gB", "<cmd>lua Snacks.picker.git_branches() <CR>", { desc = "git branches" })
 
 vim.keymap.set("i", "<C-h>", "<Left>", { desc = "Move left" })
 vim.keymap.set("i", "<C-j>", "<Down>", { desc = "Move down" })

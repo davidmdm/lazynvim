326bdfe8299f862788db0eab21bf90b9aaa24b53
David Desmarais-Michaud <desmarais.michaud.david@gmail.com>
2025-12-14 20:07:01 -0500

    update deps and add python back in

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -24,6 +24,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.zig" },
+    { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.git" },
     { import = "lazyvim.plugins.extras.lang.json" },

6617d3b7c367eb24dae0627f33782a70bb9164c5
David Desmarais-Michaud <desmarais.michaud.david@gmail.com>
2026-01-13 20:23:04 -0500

    add line numbers key options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,8 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.number = false
-vim.opt.relativenumber = false
+-- vim.opt.number = false
+-- vim.opt.relativenumber = false
 
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"

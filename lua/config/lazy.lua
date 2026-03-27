aae58f691c97205740a3e23a8dd42415f6e17e17
David Desmarais-Michaud <desmarais.michaud.david@gmail.com>
2026-03-27 14:28:20 -0400

    update deps and add claudecode extra

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -34,6 +34,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.ui.treesitter-context" },
     { import = "lazyvim.plugins.extras.ui.smear-cursor" },
     { import = "lazyvim.plugins.extras.editor.snacks_explorer" },
+    { import = "lazyvim.plugins.extras.ai.claudecode" },
     -- import/override with your plugins
     { import = "plugins" },
   },

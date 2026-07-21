7d6bc231215b76506f37d51d45c4171317ff2a21
David Desmarais-Michaud <desmarais.michaud.david@gmail.com>
2026-07-20 23:04:06 -0400

    add flume colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,6 +2,12 @@ return {
   { "rebelot/kanagawa.nvim" },
   { "Mofiqul/vscode.nvim" },
   {
+    "mitander/flume.nvim",
+    -- lazy = false,
+    -- priority = 1000,
+    -- opts = {},
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       -- colorscheme = "tokyonight-storm",
